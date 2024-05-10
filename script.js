5bc59b195f0e26e75f4d1e850a7c65767811a359
techup2024-lg6-cc <techup2024.lg6@gmail.com>
2024-05-10 04:37:10 +0000

    add CSS style

---
+++ script.js
@@ -0,0 +1,6 @@
+function buttonClick() {
+    alert("Button clicked!");
+    if (document.getElementById("important").style.visibility = "visible") {
+        document.getElementById("important").style.visibility = "hidden"
+    }
+}

2048caff66288a04feb69b3e2e899c5c01dc4583
techup2024-lg6-cc <techup2024.lg6@gmail.com>
2024-05-14 04:29:52 +0000

    Add: some js stuff

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function buttonClick() {
-    alert("Button clicked!");
+    alert("Button clicked! Hello everyone!");
     var disappearing = document.getElementById("important");
     
     if (disappearing.style.visibility === "visible") {
@@ -7,4 +7,34 @@ function buttonClick() {
     } else {
         disappearing.style.visibility = "visible";
     }
+}
+
+var age = 20
+age = 65
+
+var username = "preshant"
+var password = "techup"
+
+var isMale = false
+var isLoggedIn = false
+
+var blog_posts = ["Today was a good day", "The sky is blue", 
+"It's raining heavily now"]
+
+console.log(age)
+console.log(username)
+console.log(password)
+console.log(isMale)
+console.log(isLoggedIn)
+console.log(blog_posts)
+
+if (isMale == true) {
+    console.log("You are male")
+} else {
+    console.log("You are not male")
+}
+
+while (age < 70) {
+    console.log("Your age is " + age );
+    age = age + 1;
 }
